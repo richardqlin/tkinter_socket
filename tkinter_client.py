bde742db80fe517ca6137b3b54dcadfc96b8d82f
Richard Q Lin <richardqlin@gmail.com>
2021-12-04 11:38:51 -0800

    Update tkinter_client.py

--- tkinter_client.py
+++ tkinter_client.py
@@ -1,47 +1,38 @@
 import socket
 
 import threading
+from tkinter import *
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(("127.0.0.1",1234))
 
-host = '0.0.0.0'
+root = Tk()
 
-port = 9999
+root.title('client')
+frame= Frame(root, width=300)
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+frame.pack()
 
-s.bind((host,port))
+def send_data():
+    data = entry.get()
+    chat = Label(root, text=data, fg='red', bg='black', anchor='e')
+    chat.pack(side=TOP, fill=X)
+    s.sendall(data.encode())
+    entry.delete(0, END)
 
-s.listen(5)
-
-
-
-clients_dict={}
-
-
-def broadcast(message):
-    for c in clients_dict:
-        print(c)
-        clients_dict[c].send(message)
-
-def handle(client,name):
-    global clients_dict
+def get_data():
     while 1:
-        try:
-            message = client.recv(1024)
-            broadcast(message)
-        except:
-            client.close()
-            del clients_dict[name]
-            break
+        data = s.recv(1024).decode()
+        chat = Label(root,text = data,fg='green', bg='black', anchor='w')
+        chat.pack(side= TOP,fill=X )
+    s.close()
+
+recp = threading.Thread(target=get_data)
+recp.start()
+
+send = Button(root, text = 'send', command = send_data)
+send.pack(side =BOTTOM)
+entry = Entry(root)
+entry.pack(side=BOTTOM, fill=BOTH)
+root.mainloop()
 
 
-def receive():
-    while 1:
-        client, address = s.accept()
-        data = client.recv(1024)
-        clients_dict[data] = client
-        broadcast(data+ ' connected to the server '.encode('utf-8'))
-        thread = threading.Thread(target=handle, args = (client,data,))
-        thread.start()
-
-receive()
-print('sever run')
